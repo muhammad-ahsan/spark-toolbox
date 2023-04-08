21846d3d622fbba33b8e978febb5683d9ea88125
Muhammad Ahsan <muhammad.ahsan@ginkgo-analytics.com>
2023-04-08 22:50:28 +0200

    added typehint

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ from pyspark import HiveContext
 
 def get_spark_context(app_name: str = "Spark Application",
                       with_hive_context: bool = True) -> (SparkContext, HiveContext):
-    sc = SparkContext(appName=app_name)
+    sc: SparkContext = SparkContext(appName=app_name)
     if with_hive_context:
         sql_context = HiveContext(sc)
         return sc, sql_context
